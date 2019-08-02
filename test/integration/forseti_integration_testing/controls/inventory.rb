a09f349aa3c7ef507dbffc5015328907e8792052
Rehana Devani <rdevani@google.com>
2019-08-02 10:09:21 -0400

    Added integration testing for few Explainer commands

--- test/integration/forseti_integration_testing/controls/inventory.rb
+++ test/integration/forseti_integration_testing/controls/inventory.rb
@@ -174,4 +174,142 @@ control 'inventory' do
             command("forseti inventory purge 0").result
         end
     end
+
+    describe "Explain list members command is automated" do
+        subject do 
+            command("forseti explainer list_members --prefix rdevani")
+        end
+        before do 
+            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+            command("forseti model use model_new").result
+        end
+
+        let :inventory_id do
+            command("forseti inventory create").result
+            JSON.parse(command("forseti inventory list").stdout).fetch("id")
+        end
+
+        its("exit_status") { should eq 0 }
+        its("stdout") { should match /rdevani@google.com/ }
+        its("stderr") { should eq ""}
+        
+        after do 
+            command("forseti inventory purge 0").result
+            command("forseti model delete model_new")
+        end
+    end
+
+    describe "Explain list IAM roles command is automated" do
+        subject do 
+            command("forseti explainer list_roles --prefix roles/iam")
+        end
+        before do 
+            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+            command("forseti model use model_new").result
+        end
+
+        let :inventory_id do
+            command("forseti inventory create").result
+            JSON.parse(command("forseti inventory list").stdout).fetch("id")
+        end
+
+        its("exit_status") { should eq 0 }
+        its("stdout") { should match /roles\/iam.securityAdmin/ }
+        its("stderr") { should eq ""}
+        
+        after do 
+            command("forseti inventory purge 0").result
+            command("forseti model delete model_new")
+        end
+    end
+
+    describe "Explain list Storage roles command is automated" do
+        subject do 
+            command("forseti explainer list_roles --prefix roles/storage")
+        end
+        before do 
+            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+            command("forseti model use model_new").result
+        end
+
+        let :inventory_id do
+            command("forseti inventory create").result
+            JSON.parse(command("forseti inventory list").stdout).fetch("id")
+        end
+
+        its("exit_status") { should eq 0 }
+        its("stdout") { should match /roles\/storage.objectAdmin/ }
+        its("stderr") { should eq ""}
+        
+        after do 
+            command("forseti inventory purge 0").result
+            command("forseti model delete model_new")
+        end
+    end
+    
+    describe "Explain list IAM roleAdmin permissions command is automated" do
+        subject do 
+            command("forseti explainer list_permissions --roles roles/iam.roleAdmin")
+        end
+    
+        before(:context) do 
+            command("forseti inventory create").result
+            inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
+            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+            command("forseti model use model_new").result
+        end
+    
+        its("exit_status") { should eq 0 }
+        its("stdout") { should match /roles.create/ }
+        its("stdout") { should match /roles.delete/ }
+        its("stdout") { should match /roles.get/ }
+        its("stdout") { should match /roles.list/ }
+        its("stdout") { should match /roles.undelete/ }
+        its("stdout") { should match /roles.update/ }
+        its("stdout") { should match /resourcemanager.projects.get/ }
+        its("stdout") { should match /resourcemanager.projects.getIamPolicy/ }
+        its("stderr") { should eq ""}
+        
+        after(:context) do 
+            command("forseti inventory purge 0").result
+            command("forseti model delete model_new")
+        end
+    end
+
+    describe "Explain list IAM storage.Admin permissions command is automated" do
+        subject do 
+            command("forseti explainer list_permissions --roles roles/storage.Admin")
+        end
+    
+        before(:context) do 
+            command("forseti inventory create").result
+            inventory_id = JSON.parse(command("forseti inventory list").stdout).fetch("id")
+            command("forseti model create --inventory_index_id #{inventory_id} model_new").result
+            command("forseti model use model_new").result
+        end
+    
+        its("exit_status") { should eq 0 }
+        its("stdout") { should match /firebase.projects.get/ }
+        its("stdout") { should match /resourcemanager.projects.get/ }
+        its("stdout") { should match /resourcemanager.projects.list/ }
+        its("stdout") { should match /storage.buckets.create/}
+        its("stdout") { should match /storage.buckets.delete/}
+        its("stdout") { should match /storage.buckets.get/}
+        its("stdout") { should match /storage.buckets.getIamPolicy/ }
+        its("stdout") { should match /storage.buckets.list/}
+        its("stdout") { should match /storage.buckets.setIamPolicy/}
+        its("stdout") { should match /storage.buckets.update/ }
+        its("stdout") { should match /storage.objects.create/ }
+        its("stdout") { should match /storage.objects.delete/ }
+        its("stdout") { should match /storage.objects.get/ }
+        its("stdout") { should match /storage.objects.getIamPolicy/ }
+        its("stdout") { should match /storage.objects.list/ }
+        its("stdout") { should match /storage.objects.setIamPolicy/ }
+        its("stdout") { should match /storage.objects.update/ }
+    
+        after(:context) do 
+            command("forseti inventory purge 0").result
+            command("forseti model delete model_new")
+        end
+    end
 end
